16dc60f850a3f3e87e4f6446b299dd74c69a2356
riazali903 <riazali903.com>
2021-08-23 19:08:10 +0100

    First change

--- Writer.py
+++ Writer.py
@@ -1,25 +1,48 @@
 
 import json
+from threading import Thread
+import time
 
 
 students = []
-students.sort(key= lambda k : k['Searches'])
+#students.sort(key=lambda k: k['Searches'])
+size = (len(students))
+size= int(input("plese enter dict size : "))
 
-size = int(input("plese enter dict size : "))
-if size <= 10:
-    for i in range(size):
-        dict = {}
-        dict['ID'] = int(input('enter id : '))
-        dict['Name'] = input('enter name : ')
-        dict['Searches'] = int()
-        students.append(dict)
-        with open('record.json', 'w') as f:
+
+
+for student in range(size):
+    if len(students) <= size:
+        student = {}
+        #student['ID'] = int(input('enter id : '))
+        student['Name'] = input('enter name : ')
+        student['Searches'] = int()
+        students.append(student)
+        size +=1
+        with open('rooo.json', 'w') as f:
             json.dump(students, f,indent=4)
+
 # else:
-#     for student in students:
-#         print(min(student['Searches']))
-
-
+#     while True:
+#         contain = False
+#         search = input('enter your search')
+#         students.sort(key=lambda k: k['Searches'], reverse=True)
+#
+#         for student in students:
+#             if student['Name'] == search:
+#                 sCount = student['Searches']
+#                 students.remove(student)
+#                 contain = True
+#                 update_item = {"Name": search, "Searches": sCount + 1}
+#                 students.append(update_item)
+#
+#         if contain is False:
+#             print('not found, add new')
+#             update_item = {'Name': search, 'Searches': 0}
+#             students.append(update_item)
+#         with open('rooo.json', 'w') as f:
+#             json.dump(students, f, indent=4)
+#
 
 
 #search= int(input('enter your search'))
